4521977800f7d99cd2f8da690758c5a9563e9340
Andressa <andressa_nicolau@hotmail.com>
2025-03-31 12:32:53 -0300

    Ajustou tipagem

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,13 +12,31 @@ export class NegociacaoController {
     }
 
     adiciona(){
+        //criar uma data com base ne string .value, passada no input
+        //passamos a data recebido do value, porém a data recebida vem com hífen 1111-11-11
+        
+        //expressão regular para substituir os hífen por vírgula
+        const exp = /-/g;
+
+        //o replace substitui a expressão regular que pega todos os hífens pela vírgula
+        const date = new Date(this.inputData.value.replace(exp, ','));
+
+        //converter a quantidade
+        const quantidade = parseInt(this.inputQuantidade.value)
+
+        //converter o valor
+        const valor = parseFloat(this.inputQuantidade.value)
+
+        const negociacao = new Negociacao(date, quantidade,valor)
+
+        /*
         const negociacao = new Negociacao(
             //todo input.value retorna uma string, isso da conflito na tipagem, precisamos converter 
             this.inputData.value,
             this.inputQuantidade.value,
             this.inputValor.value
         )
-
+        */
         console.log(negociacao)
     }
 }
--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -6,7 +6,25 @@ export class NegociacaoController {
         this.inputValor = document.querySelector("#valor");
     }
     adiciona() {
-        const negociacao = new Negociacao(new Date(this.inputData.value), parseInt(this.inputQuantidade.value), parseInt(this.inputValor.value));
+        //criar uma data com base ne string .value, passada no input
+        //passamos a data recebido do value, porém a data recebida vem com hífen 1111-11-11
+        //expressão regular para substituir os hífen por vírgula
+        const exp = /-/g;
+        //o replace substitui a expressão regular que pega todos os hífens pela vírgula
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        //converter a quantidade
+        const quantidade = parseInt(this.inputQuantidade.value);
+        //converter o valor
+        const valor = parseFloat(this.inputQuantidade.value);
+        const negociacao = new Negociacao(date, quantidade, valor);
+        /*
+        const negociacao = new Negociacao(
+            //todo input.value retorna uma string, isso da conflito na tipagem, precisamos converter
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        )
+        */
         console.log(negociacao);
     }
 }
