5deddfbbabb44ff8148f40d263d3dc466541d724
Andressa <andressa_nicolau@hotmail.com>
2025-03-31 15:37:19 -0300

    array

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,33 @@
-import { Negociacao } from './negociacao.js';
+import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Negociacao[] = [];
+    //array do tipo Negociacao. Posso declarar uma classe como um tipo
+    private negociacoes:Array<Negociacao> = [];
 
-    adiciona(negociacao: Negociacao) {
-        this.negociacoes.push(negociacao);
-    }
-
-    lista(): readonly Negociacao[] {
-        return this.negociacoes;
+    //adiciona em uma lista negociacoes a negociacao cadastrada
+    adiciona(negociacao:Negociacao){
+        this.negociacoes.push(negociacao)
     }
 }
+
+//instancia a lista
+const negociacoes = new Negociacoes();
+
+//adiciona a lista instanciando uma negociacao
+negociacoes.adiciona(new Negociacao())
+
+
+/*
+const list:Array<number> = [];
+list.push(10);
+list.push('11')
+
+
+const list:Array<string> = [];
+list.push('Andressa');
+list.push('Nicolau')
+
+for(let nome of list){
+    nome.
+}
+*/
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,11 +1,26 @@
 export class Negociacoes {
     constructor() {
+        //array do tipo Negociacao. Posso declarar uma classe como um tipo
         this.negociacoes = [];
     }
+    //adiciona em uma lista negociacoes a negociacao cadastrada
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    lista() {
-        return this.negociacoes;
-    }
 }
+const negociacoes = new Negociacoes();
+negociacoes.adiciona;
+/*
+const list:Array<number> = [];
+list.push(10);
+list.push('11')
+
+
+const list:Array<string> = [];
+list.push('Andressa');
+list.push('Nicolau')
+
+for(let nome of list){
+    nome.
+}
+*/ 
