7f8ba73a2de735ddf36e8d913232690cce2f5628
Andressa <andressa_nicolau@hotmail.com>
2025-04-09 11:53:52 -0300

    ajustou valor na tabela

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -47,7 +47,7 @@ export class NegociacaoController {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value)
-        const valor = parseFloat(this.inputQuantidade.value)
+        const valor = parseFloat(this.inputValor.value)
 
         return new Negociacao(date, quantidade,valor)
     }
